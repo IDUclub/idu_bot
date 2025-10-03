fdf92c5e712af037b7090a02301e1b064ad3a5c2
Leon <100690100+LeonDeTur@users.noreply.github.com>
2025-10-03 17:08:34 +0300

    feat(index_napper): - added index for temp test - updated gitignore

--- src/common/constants/index_mapper.py
+++ src/common/constants/index_mapper.py
@@ -5,7 +5,8 @@ index_mapper = {
     'design': 'Проектная стадия',
     'construction': 'Строительная стадия',
     'operation': 'Эксплуатационная стадия',
-    'decommission': 'Ликвидационная стадия'
+    'decommission': 'Ликвидационная стадия',
+    'project': "Информация проекта"
 }
 
 reverse_index_mapper = {v: k for k, v in index_mapper.items()}
