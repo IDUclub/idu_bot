72e476fc970050835a92f271a9c3cf4a5205405d
Leon <turkov.leon-2@yandex.ru>
2026-02-16 15:17:11 +0300

    fix(dependencies): - fixed dependencies imports

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,15 +1,14 @@
-import json
 from pathlib import Path
 
 from iduconfig import Config
 
-from logs.logs_service import LogsService
 from src.common.constants.index_mapper import index_mapper, reverse_index_mapper
 from src.common.exceptions.http_exception import http_exception
 from src.common.logging.init_logs import init_logs
 from src.elastic.elastic_service import ElasticService
 from src.idu_llm.idu_llm_service import IduLLMService
 from src.llm.llm_service import LlmService
+from src.logs.logs_service import LogsService
 from src.vectorizer.vectorizer_service import VectorizerService
 
 # TODO remove to dependencies injection
